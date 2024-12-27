d2977b375cdec70e6edd6a8c5963820b7d23813e
Imani Pelton <imani@bepri.dev>
2024-12-27 15:36:37 -0500

    docs: add code documentation! (#1)
    * docs: add license
    
    * docs: add code documentation strings

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gmail Filter Generator
+This tool reads in a config file specifying email filters and generates an XML
+file that can be imported into Gmail. This removes the meticulous process of
+defining and organizing email filters within the Gmail client, which cares
+about the order you declare filters but does not give you the option to
+reorder them.
--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,13 @@
-use std::path::PathBuf;
+//! Parsing module for command-line arguments.
+use ::std::path::PathBuf;
 
-pub use clap::Parser;
+#[doc(hidden)]
+pub use ::clap::Parser;
 
+/// Command-line arguments.
 #[derive(Parser, Debug)]
 pub struct Args {
+    /// Configuration file of email filters. Supported formats: toml
     #[arg(required = true)]
     pub file: PathBuf,
 }
